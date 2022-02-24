c22e7b648780f5ae97913bd97f163cb451319128
Ashley Mattam <amattam427@gmail.com>
2022-02-23 22:57:00 -0500

    commit tree 729f4e6e1ce4902f01eec2c0b1858bf3e0cb6bc5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create, :update]
+  #resources :birds, only: [:index, :show, :create, :update, :destroy]
+
+  #since we are using all of the restful routes, we can omit "only"::
+  resources :birds 
   patch "/birds/:id/like", to: "birds#increment_likes"
 end
